7b88a6c1dad464ab628aad3b1b04a3f1b5f70934
Robert christian <kyssangel18@gmail.com>
2023-12-07 12:52:59 +0100

    modification html

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,83 @@
-const gallery = document.querySelector('.gallery')
+const gallery = document.querySelector(".gallery");
+const filtres = document.querySelector('.filtres');
 
-fetch('http://localhost:5678/api/works').then(response => response.json()).then(data =>createWorks(data))
+let works = [];
+let categories = [];
 
-const createWorks = (works) => {
-    works.forEach(work => {
-        const figure = document.createElement('figure')
-        const image = document.createElement('img')
-        const figcaption = document.createElement('figcaption')
+const getWorks = async () => {
+  const response = await fetch("http://localhost:5678/api/works")
+  const data = await response.json()
+  works = [...data]
 
-        image.src  = work.imageUrl;
-        image.alt = work.title;
-        figcaption.textContent = work.title;
-
-        figure.appendChild(image)
-        figure.appendChild(figcaption)
-
-        gallery.appendChild(figure)
-    });
 }
 
-// .classList   - .add()   - .remove()
+const getCategories = async () => {
+  const response = await fetch("http://localhost:5678/api/categories")
+  const data = await response.json()
+  categories = [...data]
+
+}
+
+const createWorks = (works) => {
+  works.forEach((work) => {
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+
+    image.src = work.imageUrl;
+    image.alt = work.title;
+    figcaption.textContent = work.title;
+
+    figure.appendChild(image);
+    figure.appendChild(figcaption);
+
+    gallery.appendChild(figure);
+  });
+};
+
+const createFilter = (category) => {
+  const button = document.createElement('button')
+
+    button.textContent = category.name
+
+    button.addEventListener('click', () => {
+
+      if (category.id === 0) {
+        gallery.innerHTML = ''
+        createWorks(works)
+         return
+      }
+
+      const filteredWorks = works.filter(work => work.categoryId === category.id)
+   
+      gallery.innerHTML = ''
+      createWorks(filteredWorks)
+    })
+
+    filtres.appendChild(button)
+}
+
+const createFilters = (categories) => {
+  createFilter({id: 0, name: 'Tous'})
+  categories.forEach(category => {
+    createFilter(category)
+  })
+}
+
+
+
+const init = async () => {
+ 
+  await getWorks()
+  await getCategories()
+  createWorks(works)
+  createFilters(categories)
+}
+
+init()
+
+
+
+
+   
+
